80a40c55f0e75c5179a5a7467e5f84dc3a09de0a
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-08-14 16:23:19 -0300

    primeiro commit

---
+++ src/main/java/com/LFsoftware/catalogoEletronico/CatalogoEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.LFsoftware.catalogoEletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoEletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LFsoftware/catalogoEletronico/CatalogoEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LFsoftware.catalogoEletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
